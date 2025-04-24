30e3a99f37154031390d2e1d7b2c9b07112b5d48
yuangenbao <769318433@qq.com>
2025-04-24 17:29:05 +0800

    feat: 引入ArcoDesign，通过插件按需引入样式、图标等

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { vitePluginForArco } from '@arco-plugins/vite-react'
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), vitePluginForArco()],
 })
